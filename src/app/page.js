3ef460d29f5df5a27e5e496351d224f75c829b48
Vishal <vishalkumar437@gmail.com>
2023-05-20 13:58:58 +0530

    Front End Push

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,11 @@
+import Login from "../../components/Login";
+import PatientRegister from "../../components/patientRegister";
+import DoctorRegister from "../../components/DoctorRegister"
+import { Axios } from "axios";
   export default function Home() {
   return (
     <div>
-      Hello 
+      <PatientRegister/>
     </div>
   );
 }
